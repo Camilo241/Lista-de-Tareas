ad7e417c76e07da2a9773683f1fb17a46203a15c
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-05-17 16:28:59 -0500

    Pasos Basicos :construction:

--- Modelo/Tarea.java
+++ Modelo/Tarea.java
@@ -1,5 +1,49 @@
 package Modelo;
 
 public class Tarea {
+    private int id;
+    private String titulo;
+    private String descripcion;
+    private String Categoria;
     
+
+    public Tarea(int id, String titulo, String descripcion, String categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        Categoria = categoria;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return Categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        Categoria = categoria;
+    }
+
 }
