36bdecd5df9f6cce16f8b029d379c4f3e34c5e0e
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-05-17 17:47:37 -0500

    Primeros pasos :construction;

---
+++ Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import Vista.VistaGeneral;
+
+public class Main {
+    public static void main(String[] args) {
+        VistaGeneral vistaGeneral = new Vista.VistaGeneral();
+        vistaGeneral.Vista();
+    }
+}
--- Vista/VistaGeneral.java
+++ Vista/VistaGeneral.java
@@ -1,5 +1,34 @@
 package Vista;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import Modelo.Tarea;
+
 public class VistaGeneral {
-    
+    private JFrame ventana;
+    private JPanel panel;
+    private JButton boton;
+    private JTextField campoTexto;
+    private Tarea tarea;
+
+    public void Vista() {
+        ventana = new JFrame("Lista de Tareas");
+        ventana.setSize(800 ,400);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel = new JPanel();
+        boton = new JButton("Agregar Tarea");
+        boton.setBounds(300 ,150, 50, 50);
+        campoTexto = new JTextField(20);
+
+        panel.add(boton);
+        panel.add(campoTexto);
+
+        ventana.add(panel);
+
+        ventana.setVisible(true);
+    }
 }
