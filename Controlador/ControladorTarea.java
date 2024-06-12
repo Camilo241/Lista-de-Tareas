eaaa16b080fe926198d10fbb2ea9e8f2bb905200
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-06-12 15:02:25 -0500

    LEctura en Tarea :construction:

--- Controlador/ControladorTarea.java
+++ Controlador/ControladorTarea.java
@@ -1,11 +1,12 @@
 package Controlador;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import Modelo.Categoria;
 import Modelo.Tarea;
 import SQL.coneccion;
 
@@ -38,8 +39,29 @@ public class ControladorTarea {
         }
     }
 
-    public List<Tarea> obtenerTareas() {
-        return listaTareas;
+    //READ
+    public List<Tarea> cargarTareas() {
+        List<Tarea> tareas = new ArrayList<>();
+        String sql = "{call SP_ConsultarTareas}";
+    
+        try (Connection conn = coneccion.getConnection();
+            CallableStatement stmt = conn.prepareCall(sql)) {
+    
+            ResultSet rs = stmt.executeQuery();
+    
+            while (rs.next()) {
+                // String nombre = rs.getString("NOMBRE_CATEGORIA");
+                // boolean estado = rs.getBoolean("ESTADO_CATEGORIA");
+                Tarea tarea = new Tarea(sql, sql, sql, false);
+                tareas.add(tarea);
+            }
+    
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error al obtener las categorías activas: " + e.getMessage());
+        }
+    
+        return tareas;
     }
 
 }
--- Vista/VistaGeneral.java
+++ Vista/VistaGeneral.java
@@ -97,7 +97,7 @@ public class VistaGeneral extends JFrame {
     }
 
     private void actualizarTabla() {
-        List<Tarea> tareas = controladorTarea.obtenerTareas();
+        List<Tarea> tareas = controladorTarea.cargarTareas();
         for (Tarea tarea : tareas) {
             modeloTabla.addRow(new Object[]{tarea.getFecha(), tarea.getTitulo(), tarea.getDescripcion(), tarea.getCategoria(), tarea.isEstado()});
         }
